9931d3cebbb121e2e2fb1158b7322273d8cf6fff
Ichi <mertisyoooo@gmail.com>
2026-07-28 00:14:33 +0300

    Create discord.js

---
+++ discord.js
@@ -0,0 +1,48 @@
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ]
+});
+
+client.once(Events.ClientReady, () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  // Ignore bots
+  if (message.author.bot) return;
+
+  // React to every message
+  try {
+    await message.react('🤍');
+  } catch (err) {
+    console.error("Couldn't react:", err);
+  }
+
+  // !isp command
+  if (message.content.toLowerCase() === "!isp") {
+    try {
+      await message.author.send(
+`Hey! Here are my links:
+
+📸 Instagram
+https://www.instagram.com/aboredloner/
+
+💻 GitHub
+https://github.com/WAAAAAHHHHH`
+      );
+
+      // Optional confirmation in the channel
+      await message.reply("🤍 Check your DMs!");
+    } catch (err) {
+      await message.reply("I couldn't send you a DM. Please enable DMs from server members.");
+    }
+  }
+});
+
+client.login("YOUR_BOT_TOKEN");
